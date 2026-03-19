606176afb776772fc25087ca2c3bb6893399eacb
Nocturnal <241546228+fullynocturnal@users.noreply.github.com>
2026-03-19 00:47:12 -0400

    Base watchOS support

---
+++ SheafWatch Watch App/SheafWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheafWatchApp.swift
+//  SheafWatch Watch App
+//
+//  Created by Kai on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SheafWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
