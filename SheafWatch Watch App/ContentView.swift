ba6181fc4aa0c75187fff4b8d9b04d56849afd43
Nocturnal <241546228+fullynocturnal@users.noreply.github.com>
2026-03-19 00:47:12 -0400

    Base watchOS support

---
+++ SheafWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheafWatch Watch App
+//
+//  Created by anon on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
