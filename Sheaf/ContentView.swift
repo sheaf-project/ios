b38a49459b8b1df2786781f859aa0d2f72515e02
Nocturnal <241546228+fullynocturnal@users.noreply.github.com>
2026-03-18 22:12:05 -0400

    Initial Commit

---
+++ Sheaf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sheaf
+//
+//  Created by Kai on 3/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sheaf/SheafApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheafApp.swift
+//  Sheaf
+//
+//  Created by Kai on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SheafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
